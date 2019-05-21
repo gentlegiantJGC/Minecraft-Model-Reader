18a804f43903f5de5a774c7c7bd295e938eac6f2
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-05-21 17:54:00 +0100

    Convert the MC Java model format to verts and triangles
    Added code to convert the loaded MC Java model format block into verticies and faces for use in a rendering engine.
    Still todo: texture support, rotation support, culling support

--- java_block_model.py
+++ java_block_model.py
@@ -3,6 +3,7 @@ import re
 from typing import List, Dict, Union
 import comment_json
 from api.block import Block
+import numpy
 
 
 class MinecraftJavaModelHandler:
@@ -63,9 +64,96 @@ class MinecraftJavaModelHandler:
 		model_path = blockstate['model']
 		rotx = blockstate.get('x', 0)
 		roty = blockstate.get('y', 0)
-		# uvunlock = blockstate.get('uvunlock', False)
+		# uvlock = blockstate.get('uvlock', False)
 
-		return self._recursive_load_block_model(namespace, model_path)
+		java_model = self._recursive_load_block_model(namespace, model_path)
+
+		triangle_model = {
+			'verts': [],
+			'texture_verts': [],
+			'faces': [],  # [[v1, v2, v3, t],]
+			'textures': []
+		}
+		textures = {}
+		texture_count = 1
+
+		# cube_face_lookup = {
+		# 	'down': (0, 2, 6, 4),
+		# 	'up': (1, 5, 7, 3),
+		# 	'north': (6, 2, 3, 7),  # TODO: work out the correct order of these last four
+		# 	'east': (4, 6, 7, 5),
+		# 	'south': (0, 4, 5, 1),
+		# 	'west': (0, 1, 3, 2)
+		# }
+		#
+		# cube_vert_lookup = {
+		# 	0: (0, 1, 2),
+		# 	1: (0, 4, 2),
+		# 	2: (0, 1, 5),
+		# 	3: (0, 4, 5),
+		# 	4: (3, 1, 2),
+		# 	5: (3, 4, 2),
+		# 	6: (3, 1, 5),
+		# 	7: (3, 4, 5),
+		# }
+
+		cube_lut = {
+			'down': ((0, 1, 2), (0, 1, 5), (3, 1, 5), (3, 1, 2)),
+			'up': ((0, 4, 2), (3, 4, 2), (3, 4, 5), (0, 4, 5)),
+			'north': ((3, 1, 5), (0, 1, 5), (0, 4, 5), (3, 4, 5)),
+			'east': ((3, 1, 2), (3, 1, 5), (3, 4, 5), (3, 4, 2)),
+			'south': ((0, 1, 2), (3, 1, 2), (3, 4, 2), (0, 4, 2)),
+			'west': ((0, 1, 2), (0, 4, 2), (0, 4, 5), (0, 1, 5))
+		}
+
+		vert_count = 0
+		for element in java_model['elements']:
+			element_vert_count = 0
+			element_tri_count = 0
+			element_faces = element.get('faces', {})
+			quad_count = sum(face in element_faces for face in ('down', 'up', 'north', 'east', 'south', 'west'))
+			verts = numpy.zeros((quad_count * 4, 3), numpy.float)
+			faces = numpy.zeros((quad_count * 2, 4), numpy.uint32)
+
+			corner1 = element.get('to', [0, 0, 0])
+			corner2 = element.get('from', [0, 0, 0])
+			corners = [
+				min(corner1[0], corner2[0]),
+				min(corner1[1], corner2[1]),
+				min(corner1[2], corner2[2]),
+				max(corner1[0], corner2[0]),
+				max(corner1[1], corner2[1]),
+				max(corner1[2], corner2[2]),
+			]
+
+			for face_dir, face_verts in cube_lut.items():
+				if face_dir in element_faces:
+					verts[element_vert_count:element_vert_count+4, :] = [[corners[ind] for ind in vert] for vert in face_verts]
+					tex = element_faces[face_dir].get('texture', None)
+					while isinstance(tex, str) and tex.startswith('#'):
+						tex = java_model['textures'].get(tex[1:], None)
+					if tex not in textures:
+						textures[tex] = texture_count
+						triangle_model['textures'].append(tex)
+						texture_count += 1
+
+					faces[element_tri_count:element_tri_count+2, :] = [[0, 1, 2, texture_count-1], [0, 2, 3, texture_count-1]]
+					faces[element_tri_count:element_tri_count + 2, :-1] += element_vert_count + vert_count
+					element_vert_count += 4
+					element_tri_count += 2
+
+			triangle_model['verts'].append(verts)
+			triangle_model['faces'].append(faces)
+			vert_count += element_vert_count
+
+		if len(triangle_model['verts']) == 0:
+			triangle_model['verts'] = numpy.zeros((0, 3), numpy.float)
+			triangle_model['faces'] = numpy.zeros((0, 4), numpy.uint32)
+		else:
+			triangle_model['verts'] = numpy.concatenate(triangle_model['verts'])
+			triangle_model['faces'] = numpy.concatenate(triangle_model['faces'])
+
+		return triangle_model
 
 	def _recursive_load_block_model(self, namespace: str, model_path: str) -> dict:
 		asset_dir = next(asset_dir_ for asset_dir_ in self._asset_dirs if os.path.isfile(os.path.join(asset_dir_, namespace, 'models', f'{model_path}.json')))
