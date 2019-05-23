08d7c4d7e0c7de8e21f1f5cb644b09f751e8feb2
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-05-23 15:59:19 +0100

    Added multipart support
    Also done some other minor changes and cleanup

--- java_block_model.py
+++ java_block_model.py
@@ -4,6 +4,20 @@ from typing import List, Dict, Union
 import comment_json
 from api.block import Block
 import numpy
+import copy
+
+
+def empty_model():
+	return copy.deepcopy(
+		{
+			'verts': {side: [] for side in ('down', 'up', 'north', 'east', 'south', 'west', None)},
+			'texture_verts': {side: [] for side in ('down', 'up', 'north', 'east', 'south', 'west', None)},
+			'faces': {side: [] for side in ('down', 'up', 'north', 'east', 'south', 'west', None)},  # [[v1, v2, v3],]
+			'textures': {side: [] for side in ('down', 'up', 'north', 'east', 'south', 'west', None)},  # [t,]
+			'texture_list': []
+		}
+	)
+
 
 cube_face_lut = {  # This maps face direction to the verticies used (defined in cube_vert_lut)
 	'down': [2, 6, 4, 0],
@@ -58,7 +72,7 @@ class MinecraftJavaModelHandler:
 
 	def get_model(self, blockstate_str: str) -> dict:
 		if blockstate_str not in self._models:
-			self._models[blockstate_str] = self._parse_model(*Block.parse_blockstate_string(blockstate_str))
+			self._models[blockstate_str] = self._parse_blockstate_link(*Block.parse_blockstate_string(blockstate_str))
 		return self._models[blockstate_str]
 
 	@staticmethod
@@ -71,7 +85,7 @@ class MinecraftJavaModelHandler:
 			'texture_list': []
 		}
 
-	def _parse_model(self, namespace: str, base_name: str, properties: Dict[str, str]) -> dict:
+	def _parse_blockstate_link(self, namespace: str, base_name: str, properties: Dict[str, str]) -> dict:
 		for asset_dir in reversed(self._asset_dirs):
 			if namespace in os.listdir(asset_dir):
 				if f'{base_name}.json' in os.listdir(os.path.join(asset_dir, namespace, 'blockstates')):
@@ -96,8 +110,56 @@ class MinecraftJavaModelHandler:
 										pass
 
 					elif 'multipart' in blockstate:
-						pass
-						# TODO
+						multi_model = empty_model()
+						vert_count = {side: 0 for side in ('down', 'up', 'north', 'east', 'south', 'west', None)}
+						texture_count = 0
+						for case in blockstate['multipart']:
+							try:
+								if 'when' in case:
+									if 'OR' in case['when']:
+										if not any(
+											all(
+												properties.get(prop, None) in val.split('|') for prop, val in prop_match.items()
+											) for prop_match in case['when']['OR']
+										):
+											continue
+									elif not all(
+										properties.get(prop, None) in val.split('|') for prop, val in case['when'].items()
+									):
+										continue
+
+								if 'apply' in case:
+									try:
+										temp_model = self._load_block_model(namespace, case['apply'])
+										for face in ('down', 'up', 'north', 'east', 'south', 'west', None):
+											multi_model['verts'][face].append(temp_model['verts'][face])
+											multi_model['texture_verts'][face].append(temp_model['texture_verts'][face])
+											multi_model['faces'][face].append(temp_model['faces'][face] + vert_count[face])
+											vert_count[face] += len(temp_model['verts'][face])
+											multi_model['textures'][face].append(temp_model['textures'][face] + texture_count)
+										multi_model['texture_list'] += temp_model['texture_list']
+										texture_count += len(temp_model['texture_list'])
+
+									except:
+										pass
+							except:
+								pass
+
+						out_model = empty_model()
+						for face in ('down', 'up', 'north', 'east', 'south', 'west', None):
+							for attr, shape, dtype in (('verts', (0, 3), numpy.float), ('texture_verts', (0, 2), numpy.float), ('faces', (0, 3), numpy.uint32)):
+								try:
+									out_model[attr][face] = numpy.vstack(multi_model[attr][face])
+								except:
+									out_model[attr][face] = numpy.zeros(shape, dtype)
+
+							texture_list, inverse = numpy.unique(multi_model['texture_list'], return_inverse=True)
+							out_model['texture_list'] = list(texture_list)
+							try:
+								out_model['textures'][face] = inverse[numpy.hstack(multi_model['textures'][face])]
+							except:
+								out_model['textures'][face] = numpy.zeros((0,), numpy.uint32)
+						return out_model
 
 		return self._missing_no()
 
@@ -113,13 +175,7 @@ class MinecraftJavaModelHandler:
 
 		java_model = self._recursive_load_block_model(namespace, model_path)
 
-		triangle_model = {
-			'verts': {side: [] for side in ('down', 'up', 'north', 'east', 'south', 'west', None)},
-			'texture_verts': {side: [] for side in ('down', 'up', 'north', 'east', 'south', 'west', None)},
-			'faces': {side: [] for side in ('down', 'up', 'north', 'east', 'south', 'west', None)},  # [[v1, v2, v3],]
-			'textures': {side: [] for side in ('down', 'up', 'north', 'east', 'south', 'west', None)},  # [t,]
-			'texture_list': []
-		}
+		triangle_model = empty_model()
 		texture_list = {}
 		texture_count = 0
 
@@ -145,9 +201,9 @@ class MinecraftJavaModelHandler:
 						texture_count += 1
 					triangle_model['textures'][cull_dir] += [texture_list[tex]] * 4
 
-					texture_uv = numpy.array(element_faces[face_dir].get('uv', [0, 0, 16, 16]), numpy.float)/16
+					texture_uv = numpy.array(element_faces[face_dir].get('uv', [0, 0, 16, 16]), numpy.float)/16  # TODO: get the uv based on box location if not defined
 					texture_rotation = element_faces[face_dir].get('rotation', 0)
-					uv_slice = uv_lut[int(texture_rotation/45):] + uv_lut[:int(texture_rotation/45)]
+					uv_slice = uv_lut[2 * int(texture_rotation/90):] + uv_lut[:2 * int(texture_rotation/90)]
 					triangle_model['texture_verts'][cull_dir].append(texture_uv[uv_slice].reshape((-1, 2)))
 
 					faces = numpy.array([[0, 1, 2], [0, 2, 3]], numpy.uint32)
@@ -161,10 +217,12 @@ class MinecraftJavaModelHandler:
 				triangle_model['verts'][cull_dir] = numpy.zeros((0, 3), numpy.float)
 				triangle_model['faces'][cull_dir] = numpy.zeros((0, 3), numpy.uint32)
 				triangle_model['texture_verts'][cull_dir] = numpy.zeros((0, 2), numpy.float)
+				triangle_model['textures'][cull_dir] = numpy.zeros((0,), numpy.uint32)
 			else:
-				triangle_model['verts'][cull_dir] = numpy.vstack(triangle_model['verts'][cull_dir])
+				triangle_model['verts'][cull_dir] = numpy.vstack(triangle_model['verts'][cull_dir])  # TODO: rotate model based on uv_lock
 				triangle_model['faces'][cull_dir] = numpy.vstack(triangle_model['faces'][cull_dir])
 				triangle_model['texture_verts'][cull_dir] = numpy.vstack(triangle_model['texture_verts'][cull_dir])
+				triangle_model['textures'][cull_dir] = numpy.array(triangle_model['textures'][cull_dir], numpy.uint32)
 
 		return triangle_model
 
@@ -187,5 +245,5 @@ class MinecraftJavaModelHandler:
 
 
 if __name__ == '__main__':
-	java_model_handler = MinecraftJavaModelHandler(['assets'])
-	print(java_model_handler.get_model('minecraft:end_portal_frame[eye=true,facing=west]'))
+	java_model_handler = MinecraftJavaModelHandler(['assets', r"PureBDcraft 512x MC113\assets"])
+	print(java_model_handler.get_model('minecraft:oak_stairs'))
