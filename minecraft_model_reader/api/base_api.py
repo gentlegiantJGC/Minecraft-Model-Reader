696c1ab93892c20a9c0c88835d96e7495f470981
gentlegiantJGC <james_clare1@yahoo.co.uk>
2019-07-12 13:39:24 +0100

    Added a base resource pack handler class and textures
    This class defines the base api that all handlers must inherit

--- minecraft_model_reader/api/base_api.py
+++ minecraft_model_reader/api/base_api.py
@@ -2,6 +2,7 @@ from typing import Dict
 import os
 
 default_pack_icon = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'image', 'missing_pack_java.png')
+missing_no = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'image', 'missing_no.png')
 
 
 class BaseRP:
@@ -40,3 +41,18 @@ class BaseRP:
 		"""str - path to the pack icon"""
 		return self._pack_icon
 
+
+class BaseRPHandler:
+	"""
+	The base class that all resource pack handlers must inherit from. Defines the base api.
+	"""
+	def __init__(self):
+		self._packs = []
+		self._textures = {}
+		self._missing_no = missing_no
+
+	def get_texture(self, path: str):
+		if path in self._textures:
+			return self._textures[path]
+		else:
+			return self._missing_no
