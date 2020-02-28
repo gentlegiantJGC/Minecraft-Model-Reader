9863af4aef8580fc1c1cea13cb7c4777c068c5c4
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-02-28 11:21:57 +0000

    Added latest resource pack as a public attribute

--- minecraft_model_reader/__init__.py
+++ minecraft_model_reader/__init__.py
@@ -2,6 +2,8 @@ import os
 from minecraft_model_reader.lib.log import log
 from .api.minecraft_mesh import MinecraftMesh
 from minecraft_model_reader.java.java_rp_handler import JavaRP, JavaRPHandler
+from .java.download_resources import get_latest
 
 path = os.path.dirname(__file__)
 java_vanilla_fix = JavaRP(os.path.join(path, 'resource_packs', 'java_vanilla_fix'))
+java_vanilla_latest = get_latest()
