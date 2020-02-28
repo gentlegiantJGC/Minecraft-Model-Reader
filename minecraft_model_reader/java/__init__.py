44c45c86bbfb73c51cb9c39aaaf62a1bcf333954
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-02-28 11:10:43 +0000

    Added some functions to download resource packs from the client.jar files

---
+++ minecraft_model_reader/java/__init__.py
@@ -0,0 +1,13 @@
+import json
+from urllib.request import urlopen
+from minecraft_model_reader import log
+
+try:
+    launcher_manifest = json.load(urlopen('https://launchermeta.mojang.com/mc/game/version_manifest.json'))
+except:
+    log.error('Failed to download the launcher manifest. This may cause problems if you have not used the program before. Make sure you are connected to the internet.')
+    launcher_manifest = {
+        "latest": {
+            "release": None
+        }
+    }
---
+++ minecraft_model_reader/java/download_resources.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+import zipfile
+import json
+from urllib.request import urlopen
+import io
+
+from . import launcher_manifest
+import minecraft_model_reader
+from minecraft_model_reader import log
+
+
+def get_latest():
+    vanilla_rp_path = os.path.join(minecraft_model_reader.path, 'resource_packs', 'java_vanilla')
+    new_version = launcher_manifest['latest']['release']
+    if new_version is None:
+        if os.path.isdir(vanilla_rp_path):
+            log.error('Could not download the launcher manifest. The resource pack seems to be present so using that.')
+        else:
+            log.error('Could not download the launcher manifest. The resource pack is not present, blocks may not be rendered correctly.')
+    else:
+        if os.path.isdir(vanilla_rp_path):
+            if os.path.isfile(os.path.join(vanilla_rp_path, 'version')):
+                with open(os.path.join(vanilla_rp_path, 'version')) as f:
+                    old_version = f.read()
+                if old_version != new_version:
+                    _remove_and_download(vanilla_rp_path, new_version)
+            else:
+                _remove_and_download(vanilla_rp_path, new_version)
+        else:
+            _remove_and_download(vanilla_rp_path, new_version)
+
+
+def _remove_and_download(path, version):
+    if os.path.isdir(path):
+        shutil.rmtree(path, ignore_errors=True)
+    if download_resources(path, version):
+        with open(os.path.join(path, 'version'), 'w') as f:
+            f.write(version)
+
+
+def download_resources(path, version) -> bool:
+    log.info(f'Downloading Java resource pack for version {version}')
+    version_url = next((v["url"] for v in launcher_manifest['versions'] if v['id'] == version), None)
+    if version_url is None:
+        log.error(f'Could not find Java resource pack for version {version}.')
+        return False
+
+    try:
+        version_manifest = json.load(urlopen(version_url))
+        version_client_url = version_manifest["downloads"]["client"]["url"]
+
+        client = zipfile.ZipFile(io.BytesIO(urlopen(version_client_url).read()))
+        for fpath in client.namelist():
+            if fpath.startswith('assets/'):
+                client.extract(fpath, path)
+        client.extract('pack.mcmeta', path)
+        client.extract('pack.png', path)
+
+    except:
+        log.error(
+            f'Failed to download and extract the Java resource pack for version {version}. Make sure you have a connection to the internet.',
+            exc_info=True
+        )
+        return False
+    return True
