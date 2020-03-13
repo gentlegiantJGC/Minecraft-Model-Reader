4ce74d0c62dbdda9d35e5309720b311ca794dcea
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2020-03-13 12:04:58 +0000

    Exposed the base resource pack classes in main init

--- minecraft_model_reader/__init__.py
+++ minecraft_model_reader/__init__.py
@@ -1,6 +1,7 @@
 import os
 from minecraft_model_reader.lib.log import log
 from .api.minecraft_mesh import MinecraftMesh
+from minecraft_model_reader.api.resource_pack import BaseRP, BaseRPHandler
 from minecraft_model_reader.java.java_rp_handler import JavaRP, JavaRPHandler
 from .java.download_resources import get_latest
 
