15b0b8a075b595c580d408c1027f37b3ccad4b6a
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-04-14 12:37:39 +0100

    Added a timeout on loading the launcher manifest
    This slows down loading if you don't have an internet connection

--- minecraft_model_reader/java/__init__.py
+++ minecraft_model_reader/java/__init__.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen
 from minecraft_model_reader import log
 
 try:
-    launcher_manifest = json.load(urlopen('https://launchermeta.mojang.com/mc/game/version_manifest.json'))
+    launcher_manifest = json.load(urlopen('https://launchermeta.mojang.com/mc/game/version_manifest.json', timeout=0.3))
 except:
     log.error('Failed to download the launcher manifest. This may cause problems if you have not used the program before. Make sure you are connected to the internet.')
     launcher_manifest = {
