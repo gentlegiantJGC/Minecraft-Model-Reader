58de3315af549edcf256d0af11f5cffbe4eb9c3c
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-09-07 16:37:38 +0100

    Added the remainder of the load manager logic

--- minecraft_model_reader/api/resource_pack/__init__.py
+++ minecraft_model_reader/api/resource_pack/__init__.py
@@ -13,4 +13,21 @@ def load_resource_pack(resource_pack_path: str):
 
 
 def load_resource_pack_manager(resource_packs: Iterable[Union[str, BaseResourcePack]]) -> BaseResourcePackManager:
-    resource_packs = [resource_pack if isinstance(resource_pack, BaseResourcePack) else load_resource_pack(resource_pack) for resource_pack in resource_packs]
+    resource_packs_out = []
+    for resource_pack in resource_packs:
+        if isinstance(resource_pack, str):
+            resource_pack = load_resource_pack(resource_pack)
+        if not isinstance(resource_pack, UnknownResourcePack) and resource_pack.valid_pack:
+            if resource_packs_out:
+                if isinstance(resource_pack, resource_packs_out[0].__class__):
+                    resource_packs_out.append(resource_pack)
+            else:
+                resource_packs_out.append(resource_pack)
+
+    resource_packs = resource_packs_out
+    if resource_packs:
+        if isinstance(resource_packs[0], JavaResourcePack):
+            return JavaResourcePackManager(resource_packs)
+
+    raise NotImplementedError
+    # return UnknownResourcePackManager()
