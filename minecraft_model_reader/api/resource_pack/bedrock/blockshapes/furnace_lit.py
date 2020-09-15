baa93882439986298d33d77931fbd3ff32004ef2
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-09-15 16:12:06 +0100

    Added a furnace block shape

---
+++ minecraft_model_reader/api/resource_pack/bedrock/blockshapes/furnace_lit.py
@@ -0,0 +1,21 @@
+from typing import Dict
+
+from minecraft_model_reader.api.resource_pack.bedrock.blockshapes.furnace import Furnace
+
+
+class FurnaceLit(Furnace):
+    @property
+    def blockshape(self) -> str:
+        return "furnace_lit"
+
+    @property
+    def rotation_map(self) -> Dict[int, int]:
+        return {
+            2: 3,
+            3: 1,
+            4: 2,
+            5: 0
+        }
+
+
+BlockShape = FurnaceLit()
