f65b8eb5306229da5686011d5961f3cb997cffe8
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-09-16 16:50:30 +0100

    Added in the education and chemistry packs to the testing

--- tests/bedrock_resource_pack_test.py
+++ tests/bedrock_resource_pack_test.py
@@ -3,8 +3,13 @@ import os
 import amulet_nbt
 
 from minecraft_model_reader.api.resource_pack.bedrock.download_resources import get_bedrock_vanilla_latest, get_bedrock_vanilla_fix
-from minecraft_model_reader.api.resource_pack import load_resource_pack_manager
+from minecraft_model_reader.api.resource_pack import load_resource_pack_manager, load_resource_pack
 from minecraft_model_reader.api import Block
+
+ExtraRP = [
+    r"C:\Program Files\WindowsApps\Microsoft.MinecraftUWP_1.16.2054.0_x64__8wekyb3d8bbwe\data\resource_packs\education",
+    r"C:\Program Files\WindowsApps\Microsoft.MinecraftUWP_1.16.2054.0_x64__8wekyb3d8bbwe\data\resource_packs\chemistry"
+]
 
 NBTMap = {
     "byte": amulet_nbt.TAG_Byte,
@@ -15,8 +20,10 @@ NBTMap = {
 
 def main():
     rp_latest = get_bedrock_vanilla_latest()
+    extra = [load_resource_pack(path) for path in ExtraRP if os.path.isdir(path)]
     rp_fix = get_bedrock_vanilla_fix()
-    rp = load_resource_pack_manager([rp_latest, rp_fix])
+    rp = load_resource_pack_manager([rp_latest, *extra, rp_fix])
+    print(rp.pack_paths)
     with open(os.path.join(os.path.dirname(__file__), "..", "minecraft_model_reader", "api", "resource_pack", "bedrock", "block_palette.json")) as f:
         palette = json.load(f)
     missing = rp.missing_block
