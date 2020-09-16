3e4cb7304c627d86fbf83502729d3490b037c1ed
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-09-16 12:48:12 +0100

    Added block models for pistons

--- minecraft_model_reader/api/resource_pack/bedrock/blockshapes/partial_block.py
+++ minecraft_model_reader/api/resource_pack/bedrock/blockshapes/partial_block.py
@@ -5,17 +5,20 @@ from minecraft_model_reader.api import Block
 from minecraft_model_reader.api.mesh.block.cube import get_cube
 from minecraft_model_reader.api.resource_pack.bedrock.blockshapes.default import Default
 
+BoundsType = Tuple[Tuple[float, float], Tuple[float, float], Tuple[float, float]]
+DoNotCullType = Tuple[bool, bool, bool, bool, bool, bool]
+
 
 class PartialBlock(Default):
-    def bounds(self, block: Block) -> Tuple[Tuple[float, float], Tuple[float, float], Tuple[float, float]]:
+    def bounds(self, block: Block) -> BoundsType:
         return (0, 1), (0, 1), (0, 1)
 
     @property
-    def do_not_cull(self) -> Tuple[bool, bool, bool, bool, bool, bool]:
+    def do_not_cull(self) -> DoNotCullType:
         return False, False, False, False, False, False
 
-    def get_block_model(self, block: Block, down: str, up: str, north: str, east: str, south: str, west: str, transparency: Tuple[bool, bool, bool, bool, bool, bool], modify_uv=True) -> BlockMesh:
-        bounds = self.bounds(block)
+    def get_block_model(self, block: Block, down: str, up: str, north: str, east: str, south: str, west: str, transparency: Tuple[bool, bool, bool, bool, bool, bool], modify_uv=True, bounds: BoundsType = None, do_not_cull: DoNotCullType = None) -> BlockMesh:
+        bounds = bounds or self.bounds(block)
         if modify_uv:
             uv = (
                 (bounds[0][0], bounds[2][0], bounds[0][1], bounds[2][1]),
@@ -37,5 +40,5 @@ class PartialBlock(Default):
             2,
             bounds=bounds,
             texture_uv=uv,
-            do_not_cull=self.do_not_cull
+            do_not_cull=do_not_cull or self.do_not_cull
         )
---
+++ minecraft_model_reader/api/resource_pack/bedrock/blockshapes/piston_arm.py
@@ -0,0 +1,40 @@
+from typing import Tuple, Dict
+
+from minecraft_model_reader.api.mesh.block import BlockMesh
+from minecraft_model_reader.api import Block
+from minecraft_model_reader.api.resource_pack.bedrock.blockshapes.partial_block import PartialBlock
+import amulet_nbt
+
+
+class PistonArm(PartialBlock):
+    def is_valid(self, block: Block) -> bool:
+        return isinstance(block.properties.get("facing_direction"), amulet_nbt.TAG_Int)
+
+    @property
+    def blockshape(self) -> str:
+        return "piston_arm"
+
+    def texture_index(self, block: Block, aux_value: int) -> int:
+        return 0
+
+    @property
+    def rotation_map(self) -> Dict[int, Tuple[int, int]]:
+        return {
+            0: (2, 0),
+            1: (0, 0),
+            2: (1, 2),
+            3: (1, 0),
+            4: (1, 1),
+            5: (1, 3)
+        }
+
+    def get_block_model(self, block: Block, down: str, up: str, north: str, east: str, south: str, west: str, transparency: Tuple[bool, bool, bool, bool, bool, bool]) -> BlockMesh:
+        rotation = self.rotation_map.get(block.properties["facing_direction"].value, (0, 0))
+
+        return BlockMesh.merge([
+            super().get_block_model(block, down, up, north, north, north, north, transparency, bounds=((0, 1), (12/16, 1), (0, 1)), do_not_cull=(True, False, True, True, True, True)),
+            super().get_block_model(block, west, west, west, west, west, west, (transparency[5],)*6, bounds=((6/16, 10/16), (0, 12/16), (6/16, 10/16)), do_not_cull=(False, False, True, True, True, True))
+        ]).rotate(*rotation)
+
+
+BlockShape = PistonArm()
