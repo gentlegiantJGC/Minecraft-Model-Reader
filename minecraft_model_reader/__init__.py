86f34d6340c9ccef1d7efbb81ba267e7b3c7fd67
gentlegiantJGC <james_clare1@yahoo.co.uk>
2020-11-14 12:06:05 +0000

    Added versioneer and modified setup.py

--- minecraft_model_reader/__init__.py
+++ minecraft_model_reader/__init__.py
@@ -7,3 +7,8 @@ from minecraft_model_reader.api.resource_pack import (
 )
 
 path = os.path.dirname(__file__)
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
