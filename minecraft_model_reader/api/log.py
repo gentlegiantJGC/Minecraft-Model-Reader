3517eb8b2643cabd4b84e9ba6a57219742cdb0ff
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2022-04-20 10:39:14 +0100

    Improved logging support
    Removed default file handler and only added default console handler if no handler has been added.
    Switched to using `logging.getLogger` to get the logger.

--- minecraft_model_reader/api/log.py
+++ minecraft_model_reader/api/log.py
@@ -1,25 +1,32 @@
 import logging
 import sys
-import os
+
+"""
+By default the logger only logs to the console.
+If you wish to modify the handlers you should
+1) Get the logger
+2) Remove and close all existing handlers
+3) Set up the logger how you desire
+
+Step 2 may not be required if you set up the logger before this module is imported
+but there is no harm in doing it and if you don't everything may get logged to the console twice.
+"""
+
 
 log = logging.getLogger("minecraft_model_reader")
-log.setLevel(logging.INFO)
 
-_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
-os.makedirs("./logs", exist_ok=True)
-_log_file = logging.FileHandler(
-    "./logs/minecraft_model_reader.log", "w", encoding="utf-8"
-)
-if "amulet-debug" in sys.argv:
-    _log_file.setLevel(logging.DEBUG)
-    log.setLevel(logging.DEBUG)
-else:
-    _log_file.setLevel(logging.INFO)
-_log_file.setFormatter(_formatter)
-log.addHandler(_log_file)
+def _init_logging():
+    if not log.handlers:
+        log.setLevel(logging.DEBUG if "amulet-debug" in sys.argv else logging.INFO)
 
-_log_console = logging.StreamHandler()
-_log_console.setLevel(logging.INFO)
-_log_console.setFormatter(_formatter)
-log.addHandler(_log_console)
+        formatter = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+        )
+
+        console_handler = logging.StreamHandler()
+        console_handler.setFormatter(formatter)
+        log.addHandler(console_handler)
+
+
+_init_logging()
