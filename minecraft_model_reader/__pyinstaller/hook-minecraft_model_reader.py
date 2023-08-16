e0ccd956e63a2dcc98a01d79f76817a7131de286
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2023-08-16 10:47:21 +0100

    Added hidden imports (#27)

--- minecraft_model_reader/__pyinstaller/hook-minecraft_model_reader.py
+++ minecraft_model_reader/__pyinstaller/hook-minecraft_model_reader.py
@@ -1,3 +1,4 @@
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
 datas = collect_data_files("minecraft_model_reader")
+hiddenimports = collect_submodules("minecraft_model_reader")
